5e766a51686edb3523238eafcc4369c5ee78a68a
GuestUser <GuestUser@Carolyns-MacBook-Pro.local>
2021-10-24 13:29:45 -0400

    updated the graph to be a line graph rather than a scatter plot

--- visualization.R
+++ visualization.R
@@ -10,11 +10,13 @@
 #   return(tmp)
 # }
 
-
-#Instead of having two functions, try combining the functions into 1 and make the yvar apart of the function. 
-#This way you can create one function, but call it twice. The user will just need to specify the data and the y - variable.
+#Summary of changes
+  #Instead of having two functions, try combining the functions into 1 and make the yvar apart of the function. 
+  #This way you can create one function, but call it twice. The user will just need to specify the data and the y - variable.
+  
+  #Additionally changed the chart to the line chart
 plot3 <- function(t,yvar){
-  tmp<-ggplot(data = test)+geom_point(mapping=aes(x=year,y={{yvar}},color=country))
+  tmp<-ggplot(data = t)+geom_line(mapping=aes(x=year,y={{yvar}},color=country))
   return(tmp)
 }
 
