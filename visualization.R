a7984dc010fd7b881b1b661cf1e995cd4090471f
GuestUser <GuestUser@Carolyns-MacBook-Pro.local>
2021-10-24 13:27:48 -0400

    combined the two functions into one to reduce the amount of code needed in the visualization

--- visualization.R
+++ visualization.R
@@ -1,14 +1,20 @@
 # set function for plotting babies per woman
-plot1 <- function(t){
-  tmp<-ggplot(data = t)+geom_point(mapping=aes(x=year,y=child_per,color=country))
-  return(tmp)
-}
+# plot1 <- function(t){
+#   tmp<-ggplot(data = t)+geom_point(mapping=aes(x=year,y=child_per,color=country))
+#   return(tmp)
+# }
 
 # set function for plotting child mortality(0-5 year-olds dying per 1000 born)
-plot2 <- function(t2){
-  tmp<-ggplot(data = t2)+geom_point(mapping=aes(x=year,y=child_mortality,color=country))
+# plot2 <- function(t2){
+#   tmp<-ggplot(data = t2)+geom_point(mapping=aes(x=year,y=child_mortality,color=country))
+#   return(tmp)
+# }
+
+
+#Instead of having two functions, try combining the functions into 1 and make the yvar apart of the function. 
+#This way you can create one function, but call it twice. The user will just need to specify the data and the y - variable.
+plot3 <- function(t,yvar){
+  tmp<-ggplot(data = test)+geom_point(mapping=aes(x=year,y={{yvar}},color=country))
   return(tmp)
 }
-
-
 
